6575a19c29f6b6b9482d92ed71fa49f056000306
Alex Davis <8m3xlt@gmail.com>
2023-01-11 15:17:11 +0000

    dietary fields

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, IntegerField, SubmitField, FieldList
+from wtforms import StringField, SelectField, IntegerField, SubmitField, BooleanField
 from wtforms.validators import NumberRange
 
 
 class RecommenderForm(FlaskForm):
     ingredients = (StringField("Ingredient"))
-    diet = SelectField("Dietary Requirements", choices=[
+    diet = SelectField("Veg, pesc, etc", choices=[
+        (None, 'None'),
         ('vgn', 'Vegan'),
         ('veg', 'Vegetarian'),
         ('psc', 'Pescetarian')
     ])
     max_time = IntegerField("Maximum Cooking Time", validators=[NumberRange(min=1)])
-    meal_type = StringField("Meal Type")
+
+    exclude_celery = BooleanField("Celery")
+    exclude_dairy = BooleanField("Dairy")
+    exclude_eggs = BooleanField("Eggs")
+    exclude_gluten = BooleanField("Gluten")
+    exclude_mustard = BooleanField("Mustard")
+    exclude_nuts = BooleanField("Nuts")
+    exclude_sesame = BooleanField("Sesame")
+    exclude_soya = BooleanField("Soya")
+
     submit = SubmitField("Find Recipes")
