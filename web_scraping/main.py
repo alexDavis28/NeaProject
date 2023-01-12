5edb01778d151dc05f5661fbaac37b0835f9d653
8m3xl <8m3xlt@gmail.com>
2023-01-12 14:49:07 +0000

    Began mass scraping

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -1,7 +1,23 @@
-from app import models
-import scrapers
+import requests
+from scrapers import AllRecipes
+url = "https://www.allrecipes.com/"
+scraper = AllRecipes()
 
-scraper = scrapers.AllRecipes()
-url = "https://www.allrecipes.com/recipe/57783/emilys-famous-hash-browns/"
-recipe = scraper.scrape_page(url)
-print(recipe.__dict__)
+soup = scraper.make_soup(url)
+
+links = soup.find(id="header-nav_1-0").findAll("a")
+links = [l.get("href") for l in links]
+
+links_to_scrape = []
+for link in links:
+    try:
+        recipe_links = scraper.find_recipe_links(link)
+        for recipe_link in recipe_links:
+            links_to_scrape.append(recipe_link)
+    except Exception as e:
+        print(e)
+
+# 3908
+
+# TODO https://www.allrecipes.com/recipes-a-z-6735880
+# TODO https://www.allrecipes.com/ingredients-a-z-6740416
