c5cb1e1aff81d1de68b7156168852f92e72bce10
Alex Davis <8m3xlt@gmail.com>
2023-01-22 18:38:55 +0000

    Fixed typ

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, session, redirect, url_for
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    """Home page of the site, displaying the reccomender form"""
+    """Home page of the site, displaying the recommender form"""
     form = RecommenderForm()
     if form.validate_on_submit():
         data = {
