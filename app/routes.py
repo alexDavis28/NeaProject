9250f9203ec0faca62cd0d34c96d74ac38d827dc
Alex Davis <8m3xlt@gmail.com>
2023-01-25 20:26:13 +0000

    Show results of query

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, database
 from app.forms import RecommenderForm
 from flask import render_template, session, redirect, url_for
+from app.models import Query
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -8,18 +9,19 @@ def index():
     """Home page of the site, displaying the recommender form"""
     form = RecommenderForm()
     if form.validate_on_submit():
-        data = {
-            "Ingredient": form.ingredients.data,
-            "MaxTime": form.max_time.data,
-        }
-        session["recommenderFormData"] = data
+        query = Query(form.ingredients.data, form.max_time.data)
+
+        session["query"] = query.__dict__
         return redirect(url_for("recommend"))
     return render_template("index.html", form=form)
 
 
 @app.route("/recommend")
 def recommend():
-    return session["recommenderFormData"]
+    query_data = session["query"]
+    query = Query(query_data["raw_ingredients"], query_data["max_time"])
+    results = database.select_recipes_with_query(query)
+    return render_template("recommend.html", results=results)
 
 
 @app.route('/info')
