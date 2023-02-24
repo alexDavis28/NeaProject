aa5a21487bcd9d3b4e7d17812ffc79e469c81612
Alex Davis <8m3xlt@gmail.com>
2023-02-24 20:55:07 +0000

    recipe saving

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from app import app, recommender
-from app.forms import RecommenderForm, CreateProfileForm, LoginForm
+from app.forms import RecommenderForm, CreateProfileForm, LoginForm, SaveRecipeForm
 from flask import render_template, session, redirect, url_for, request, flash
 from app.models import Query, User
-from app.database import add_profile_to_database, find_user_by_email
+from app.database import add_profile_to_database, find_user_by_email, user_save_recipe
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -18,15 +18,34 @@ def index():
     return render_template("index.html", form=form)
 
 
-@app.route("/recommend")
+@app.route("/recommend", methods=["GET", "POST"])
 def recommend():
-    query_data = session["query"]
-    query = Query(raw_ingredients=query_data["raw_ingredients"], sort_mode=query_data["sort_mode"], max_time=query_data[
-        "max_time"], limit=query_data["limit"])
-    query.results = recommender.find_results(query)
-    # Limit number returned
-    query.results = query.results[:query.limit]
-    return render_template("recommend.html", results=query.results, query_ingredients=query.cleaned_tokens)
+    if "query" in session:
+        form = SaveRecipeForm()
+        query_data = session["query"]
+        query = Query(raw_ingredients=query_data["raw_ingredients"], sort_mode=query_data["sort_mode"],
+                      max_time=query_data["max_time"], limit=query_data["limit"])
+        query.results = recommender.find_results(query)
+        # Limit number returned
+        query.results = query.results[:query.limit]
+        return render_template("recommend.html", results=query.results, query_ingredients=query.cleaned_tokens,
+                               form=form)
+    else:
+        return redirect("/")
+
+
+@app.route("/save_recipe", methods=["POST"])
+def save_recipe():
+    if "active_user_email" in session:
+        if request.method == "POST":
+            print("e")
+            print(request.form["recipe_title"])
+            user = find_user_by_email(session["active_user_email"])
+            user_save_recipe(request.form["recipe_title"], user)
+        return redirect("/profile")
+    else:
+        flash("Please log in to save recipes", "info")
+        return redirect("login")
 
 
 @app.route('/info')
@@ -62,30 +81,33 @@ def login():
         email = form.email.data
         password = form.password.data
         stored_user = find_user_by_email(email)
-        login_user = User(first_name=stored_user.first_name, last_name=stored_user.last_name, email=email,
-                          plaintext_password=password)
-        login_user.password_hash = login_user.calculate_password_hash()
-        if login_user.password_hash == stored_user.password_hash:
-            session["active_user"] = email
-            return redirect("/profile")
+        if not stored_user:
+            flash("Invalid email", "form")
         else:
-            flash("Invalid email or password", "form")
+            login_user = User(first_name=stored_user.first_name, last_name=stored_user.last_name, email=email,
+                              plaintext_password=password)
+            login_user.password_hash = login_user.calculate_password_hash()
+            if login_user.password_hash == stored_user.password_hash:
+                session["active_user_email"] = email
+                return redirect("/profile")
+            else:
+                flash("Invalid password", "form")
     return render_template("login.html", form=form)
 
 
 @app.route("/logout")
 def logout():
     # Logout user
-    if "active_user" in session:
-        del session["active_user"]
+    if "active_user_email" in session:
+        del session["active_user_email"]
     return redirect("/")
 
 
 @app.route("/profile")
 def profile():
-    if "active_user" in session:
+    if "active_user_email" in session:
         # find currently logged in account
-        user = find_user_by_email(session["active_user"])
+        user = find_user_by_email(session["active_user_email"])
         return render_template("profile.html", user=user)
     else:
         return redirect("login")
